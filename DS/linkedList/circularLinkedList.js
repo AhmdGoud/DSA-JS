c210f3f483d69e443d7306cb46efafecfd06054c
ahmed gouda <ag021055@gmail.com>
2026-05-24 15:46:00 +0300

    circular Linked List

--- DS/linkedList/circularLinkedList.js
+++ DS/linkedList/circularLinkedList.js
@@ -5,7 +5,7 @@ class Node {
   }
 }
 
-class SinglyLinkedList {
+class CircularLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
@@ -21,6 +21,7 @@ class SinglyLinkedList {
     } else {
       this.tail.next = newNode;
       this.tail = newNode;
+      newNode.next = this.head;
     }
 
     this.counter++;
@@ -41,86 +42,112 @@ class SinglyLinkedList {
   }
 
   delete(val) {
-    if (this.counter === 0) return;
+    if (!this.head) return;
 
     if (this.head.value === val) {
-      this.head = this.head.next;
-      this.counter--;
-      return;
+      if (this.head === this.tail) {
+        this.head = null;
+        this.tail = null;
+        return;
+      } else {
+        this.head = this.head.next;
+        return;
+      }
     }
 
     let previous = this.head;
     let current = this.head.next;
 
-    while (current !== null) {
+    do {
       if (current.value === val) {
         previous.next = current.next;
         current.next = null;
         this.counter--;
+        return; // we should returned here as we assined current.next = null
       }
 
       previous = current;
-      current = current.next;
-    }
+      current = current.next; // so here current = null, and in check current.value = error
+    } while (current !== this.head);
   }
 
   search(val) {
+    if (!this.head) return;
+
     let current = this.head;
     let position = 0;
 
-    while (current !== null) {
+    do {
       if (current.value === val) {
         console.log("value is " + current.value, "and position is " + position);
+        return;
       }
 
       position += 1;
       current = current.next;
-    }
+    } while (current !== this.head);
   }
 
   reverse() {
+    if (!this.head || this.head === this.tail) return;
+
     this.tail = this.head;
 
     let previous = null;
     let current = this.head;
 
-    while (current !== null) {
+    do {
       const nextNode = current.next;
       current.next = previous;
       previous = current;
       current = nextNode;
-    }
+    } while (current !== this.tail);
 
     this.head = previous;
+    this.tail.next = this.head;
   }
 
   print() {
+    if (!this.head) {
+      console.log([]);
+      return;
+    }
+
     let current = this.head;
     const arr = [];
 
-    while (current !== null) {
+    // while (current !== this.tail) {
+    // // -- starts with while here wont print the last element
+    // // -- as it checks if current = tail it stops, so we will use do while
+    // arr.push(current.value);
+    // current = current.next;
+    // }
+
+    do {
+      // do will work first then checks so when it returns to head it will stop
+      // as do runs the code for the first time and checks if true still running
       arr.push(current.value);
       current = current.next;
-    }
+    } while (current !== this.head);
 
     console.log(arr);
   }
 
   size() {
-    console.log(this.head, this.tail);
-    return this.counter;
+    console.log(this.head);
+    console.log(this.tail);
   }
 }
 
-const sLL = new SinglyLinkedList();
+const cLL = new CircularLinkedList();
 
-sLL.prepend(4);
-sLL.prepend(2);
-sLL.append(6);
-sLL.append(8);
+cLL.prepend(4);
+cLL.prepend(2);
+cLL.append(6);
+cLL.append(8);
 
-// sLL.delete(6);
-// sLL.search(8);
-// sLL.reverse();
-sLL.print();
-// console.log(sLL.size());
+cLL.reverse();
+cLL.delete(6);
+cLL.search(2);
+cLL.print();
+cLL.size();
